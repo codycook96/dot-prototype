79d178f021249915cc618f36e257bffa9e7915e8
codycook96 <codycook96@gmail.com>
2024-08-23 12:01:49 +0200

    Adding basic JSON read/write

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+//HTML Variable Definitions
+const dotAddTextbox = document.getElementById("dot-add-textbox");
+/*const dotAddButton = document.getElementById("dot-add-button");*/
+const dotViewList = document.getElementById("dot-view-list");
+const dotReadView = document.getElementById("dot-reader-textarea");
+
+let dots = ["test1", "test2"];
+
+function addDot(){
+    if(dotAddTextbox.value === ''){
+        alert("You must enter some text!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = dotAddTextbox.value;
+        dotViewList.appendChild(li);
+        let mySpan = document.createElement("span");
+        mySpan.innerHTML = "\u00d7";
+        li.appendChild(mySpan);
+    }
+    dotAddTextbox.value = "";
+}
+
+dotViewList.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
+function readDots(){
+    fetch('./dots.json')
+        .then(function(response) { return response.json(); })
+        .then(function(json) {
+        dots = json.dots
+        //dotReadView.value = json.dots[0];
+    });
+    
+}
+   
+function displayDots(){
+    let dotString = "";
+    dots.forEach(dot => {
+        dotString = dotString + dot + "\n"
+    });
+    dotReadView.value = dotString;
+    
+}    
---
+++ scripts/modules/parser.js
@@ -0,0 +1,11 @@
+function readDots(path){
+    let sample = require(path);
+    console.log(data);
+}
+
+function writeDots(path){
+
+}
+
+
+export { readDots, writeDots };
